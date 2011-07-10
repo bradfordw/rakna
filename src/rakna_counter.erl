405e012839f49566f759b4c57b7173b48cd8a499
Bradford Winfrey <bradswinfrey@yahoo.com>
2011-07-10 11:48:24 -0500

    Incremental counters now work. Need to add a few more functions for API. Decremental functions next, then (hopefully) onto distribution. I may make it after all.

--- src/rakna_counter.erl
+++ src/rakna_counter.erl
@@ -2,39 +2,51 @@
 
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/0, get_counter/1, get_counter/2]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(counter_state, {level_ref}).
 
+%% API
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+get_counter(Key) when is_binary(Key) ->
+	gen_server:call(rakna_counter, {get, Key}).
+
+get_counter(Date, Key) when is_tuple(Date) ->
+	gen_server:call(rakna_counter, {get, Date, Key}).
+
+%% gen_server exports
 init([]) ->
 	{ok, LevelDbPath} = application:get_env(rakna_leveldb_path),
-	{ok, Ref} = open(LevelDbPath, [{create_if_missing, true}]),
+	{ok, Ref} = eleveldb:open(LevelDbPath, [{create_if_missing, true}]),
 	{ok, #counter_state{level_ref = Ref}}.
 
+handle_call({get, Key}, _From, State) ->
+	K = term_to_binary({date(), Key}),
+	{ok, CurrentValue} = get(State#counter_state.level_ref, K),
+	{reply, CurrentValue, State};
+handle_call({get, Date, Key}, _From, State) ->
+	K = term_to_binary({Date, Key}),
+	{ok, CurrentValue} = get(State#counter_state.level_ref, K),
+	{reply, CurrentValue, State};
 handle_call({incr, Key}, _From, State) ->
-	incr(Key, State#counter_state.level_ref);
-handle_call({incr, Date, Key}, _From, State) ->
-	incr(Date, Key, State#counter_state.level_ref);
+	R = incr(Key, State#counter_state.level_ref),
+	{reply, R, State};
 handle_call({incrby, Key, Amount}, _From, State) ->
-	incrby(Key, Amount, State#counter_state.level_ref);
-handle_call({incrby, Date, Key, Amount}, _From, State) ->
-	incrby(Date, Key, Amount, State#counter_state.level_ref);
+	R = incr(Key, Amount, State#counter_state.level_ref),
+	{reply, R, State};
 handle_call(_Request, _From, State) ->
 	{noreply, ok, State}.
 
 handle_cast({incr, Key}, State) ->
-	incr(Key, State#counter_state.level_ref);
-handle_cast({incr, Date, Key}, State) ->
-	incr(Date, Key, State#counter_state.level_ref);
+	ok = incr(Key, State#counter_state.level_ref),
+	{noreply, State};
 handle_cast({incrby, Key, Amount}, State) ->
-	incrby(Key, Amount, State#counter_state.level_ref);
-handle_cast({incrby, Date, Key, Amount}, State) ->
-	incrby(Date, Key, Amount, State#counter_state.level_ref);
+	incr(Key, Amount, State#counter_state.level_ref),
+	{noreply, State};
 handle_cast(_Msg, State) ->
 	{noreply, State}.
 
@@ -50,28 +62,37 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Functions %%
 
 incr(Key, Ref) ->
-	incr(date(), Key, 1, Ref).
+	incr(date(), Key, 1.0, Ref).
+
+incr(Key, Amount, Ref) ->
+	incr(date(), Key, Amount, Ref).
 
 incr(Date, Key, Amount, Ref) ->
-%% TODO: use sext on {Date, Key} to make eleveldb happier (sequential keys)
 	BinKey = term_to_binary({Date, Key}),
 	{ok, CurrentValue} = get(Ref, BinKey),
-	NewValue = Current + Amount,
+	NV = float_to_list(CurrentValue + Amount),
+	NewValue = list_to_binary(NV),
 	ok = eleveldb:put(Ref, BinKey, NewValue, []).
 
-decr(Key, Ref) ->
-	decr(date(), Key, 1, Ref).
-
-decr(Date, Key, Amount, Ref) ->
-%% TODO: use sext on {Date, Key} ... 
-	BinKey = term_to_binary({Date, Key}),
-	{ok, CurrentValue} = get(Ref, BinKey),
-	NewValue = Current + Amount,
-	ok = eleveldb:put(Ref, BinKey, NewValue, []).
+% decr(Key, Ref) ->
+% 	decr(date(), Key, 1.0, Ref).
+% 
+% decr(Key, Amount, Ref) ->
+% 	decr(date(), Key, Amount, Ref).
+% 
+% decr(Date, Key, Amount, Ref) ->
+% 	BinKey = term_to_binary({Date, Key}),
+% 	{ok, CurrentValue} = get(Ref, BinKey),
+% 	NewValue = CurrentValue + Amount,
+% 	ok = eleveldb:put(Ref, BinKey, NewValue, []).
 
 get(Ref, Key) ->
+	BinKey = case Key of
+		K when is_binary(K) -> K;
+		K when is_tuple(K) -> sext:encode(Key)
+	end,
 	case eleveldb:get(Ref, BinKey, []) of
 		{ok, V} ->
-			{ok, V};
-		_ -> {ok, 0}
+			{ok, list_to_float(binary_to_list(V))};
+		_ -> {ok, 0.0}
 	end.
