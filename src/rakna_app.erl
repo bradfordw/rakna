36d893a29aaaf584f68f2684ef243822f95f9a6f
Bradford Winfrey <bradswinfrey@yahoo.com>
2011-08-22 19:10:34 -0500

    ping nodes when starting up.

--- src/rakna_app.erl
+++ src/rakna_app.erl
@@ -10,6 +10,16 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+  {ok, RaknaOptions} = application:get_env(rakna_options),
+  case rakna_utils:exists(nodes, RaknaOptions) of
+    true ->
+      case proplists:get_value(nodes, RaknaOptions) of
+        [] -> ok;
+          Nodes when is_list(Nodes) ->
+        [net_adm:ping(Node) || Node <- Nodes]
+      end;
+    false -> ok
+  end,
   rakna_sup:start_link(),
   rakna_web_sup:start_link().
 
